61a2ae35b763209f58cdd6982bb4e7abd7e5dfc5
leejinwoo <leejinwoo1126@gmail.com>
2025-05-06 19:59:26 +0900

    feat: website 접속 대기 웹 페이지 개발

--- website/src/main/java/com/example/website/WebsiteApplication.java
+++ website/src/main/java/com/example/website/WebsiteApplication.java
@@ -2,6 +2,7 @@ package com.example.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 
 @SpringBootApplication
 public class WebsiteApplication {
