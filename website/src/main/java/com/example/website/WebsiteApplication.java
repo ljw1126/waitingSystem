a4b3dcf79fd80fc04a6430d3284b37d28c2f261e
leejinwoo <leejinwoo1126@gmail.com>
2025-05-09 20:52:50 +0900

    refactor: import optimize

--- website/src/main/java/com/example/website/WebsiteApplication.java
+++ website/src/main/java/com/example/website/WebsiteApplication.java
@@ -2,7 +2,6 @@ package com.example.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 
 @SpringBootApplication
 public class WebsiteApplication {
